e8429f35629b31910cb75cd90bf8d7aade237afc
ethan-bit <ethan-bit@protonmail.com>
2020-07-07 22:28:03 +0800

    update config

--- service/src/main/java/io/czz/explorer/constants/InbConstants.java
+++ service/src/main/java/io/czz/explorer/constants/InbConstants.java
@@ -4,6 +4,8 @@ public class InbConstants {
 
 //    public final static String URL = "http://119.3.211.135:8334";
     public final static String URL = "http://127.0.0.1:8334";
+    public final static String NODE_URL_USERNAME = "root";
+    public final static String NODE_URL_PASSWD = "admin";
 //    public final static String URL = "http://192.168.1.186:8334";
 //    public final static String URL = "http://192.168.1.186:19526";
 
