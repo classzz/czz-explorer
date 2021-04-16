7dd0d10391497acfc5c2b166008fe592d5b114c7
任铭亮 <rliangzi@gmail.com>
2021-04-16 14:30:33 +0800

    update confirmhash

--- service/src/main/java/io/czz/explorer/constants/InbConstants.java
+++ service/src/main/java/io/czz/explorer/constants/InbConstants.java
@@ -3,9 +3,11 @@ package io.czz.explorer.constants;
 public class InbConstants {
 
 //    public final static String URL = "http://119.3.211.135:8334";
-    public final static String URL = "http://39.103.172.193:8334";
+//    public final static String URL = "http://39.103.172.193:8334";
+    public final static String URL = "http://127.0.0.1:8334";
     public final static String NODE_URL_USERNAME = "root";
-    public final static String NODE_URL_PASSWD = "admin";
+    //public final static String NODE_URL_PASSWD = "admin";
+    public final static String NODE_URL_PASSWD = "VMRRXJ9afvNrU7Kb";
 //    public final static String URL = "http://192.168.1.186:8334";
 //    public final static String URL = "http://192.168.1.186:19526";
 
